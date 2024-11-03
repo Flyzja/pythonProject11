8f042b1d00329696bf9935a75f6bedd61e999437
Flyzja <mamontova-75@mail.ru>
2024-11-03 23:08:50 +0300

    Интроспекция2

--- module_11_2.py
+++ module_11_2.py
@@ -1,33 +1,49 @@
 from inspect import isgenerator, isgeneratorfunction, ismethodwrapper, istraceback
 import inspect
+
 help(inspect)
 
-class Introspection():
+def introspection_info(obj):
+    return obj
 
-    def __init__(self, obj):
-        self.obj = obj
+def introspection_info(obj):
+    result = {}
+    result['type'] = type(obj)
+    result['attributes'] = [attr for attr in dir(obj) if not callable(getattr(obj, attr, 'Атрибут не найден'))]
+    result['methods'] = [method for method in dir(type(obj)) if callable(getattr(obj, method))]
+    result['module'] = inspect.getmodule(introspection_info)
+    result['method'] = [m for m in inspect.getmembers(obj, predicate=inspect.isfunction)]
+    return result
 
-    def introspection_info(self):
-        pass
 
-number_info = Introspection(42)
+number_info = introspection_info(42)
 print(number_info)
+
+str_info = introspection_info('42')
+print(str_info)
+
+dict_info = introspection_info({'имя': 'Иван', 'возраст':  '45', 'профессия': 'механизатор'})
+print(dict_info)
+
+list_info = introspection_info(['Иван', 45, 32.08, (True, None), False])
+print(list_info)
+
+
+
 print(dir(number_info))
 print(isinstance('obj', int))
-print(hasattr(number_info, 'obj'))
+print(hasattr(str_info, '42'))
 print(hasattr(number_info, 'x'))
-print(getattr(number_info, 'obj'))
-print(setattr(number_info, 'obj', 10))
-print(number_info.obj)
-print(type(number_info.obj))
+
+print(vars(introspection_info)) #возвращает словарь из атрибута dict
+print(callable(introspection_info)) #является ли объект вызываемым
+print(inspect.getmembers((introspection_info)))
+
+
 print(isgenerator('obj'))  #является ли генератором
-print(isgeneratorfunction('obj')) #является ли данное значение функцией генератора
-print(ismethodwrapper('obj')) #является ли оберткой
-print(istraceback('obj')) #
-number_info = Introspection("Солнце")
+print(isgeneratorfunction(dict_info)) #является ли данное значение функцией генератора
+print(ismethodwrapper(introspection_info)) #является ли оберткой
+print(istraceback(introspection_info)) #обратная связь
+number_info = introspection_info("Солнце")
 print(isinstance("Солнце", str))
 print(isinstance("Солнце", int))
-print(getattr(number_info, 'obj'))
-print(hasattr(number_info, 'obj'))
-print(setattr(number_info, 'obj', 'эхо'))
-print(number_info.obj)
